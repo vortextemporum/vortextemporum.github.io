afd05aa6d1005dc8ef13fc4a45eac3f34752b56d
Berk Özdemir <berkozdemir@Berks-MacBook-Pro.local>
2018-06-14 14:00:03 +0300

    fix

--- letters/script.js
+++ letters/script.js
@@ -1,3 +1,4 @@
+new p5();
 // Inspired by http://www.c3.hu/collection/form/
 // Made by Berk Özdemir for personal use
 
@@ -14,38 +15,49 @@ const letterz = {
     g: ['0110', '1001', '1000', '1011', '0111'],
     h: ['1001', '1001', '1111', '1001', '1001'],
     ı: ['111', '010', '010', '010', '111'],
-    i: ['010', '000', '111', '010', '010', '010', '111'],
+    i: ['010', '---', '111', '010', '010', '010', '111'],
     j: ['0011', '0001', '0001', '1001', '0110'],
     k: ['1001', '1010', '1100', '1010', '1001'],
     l: ['1000', '1000', '1000', '1000', '1111'],
     m: ['10001', '11011', '10101', '10001', '10001'],
     n: ['1001', '1101', '1011', '1001', '1001'],
     o: ['0110', '1001', '1001', '1001', '0110'],
-    ö: ['1001', '0110', '1001', '1001', '1001', '0110'],
+    ö: ['1001', '----', '0110', '1001', '1001', '1001', '0110'],
     p: ['1110', '1001', '1110', '1000', '1000'],
     q: ['0110', '1001', '1001', '1001', '01111'],
     r: ['1110', '1001', '1110', '1010', '1001'],
     s: ['1111', '1000', '1111', '0001', '1111'],
     t: ['11111', '00100', '00100', '00100', '00100'],
     u: ['1001', '1001', '1001', '1001', '1111'],
+    ü: ['1001', '----', '1001', '1001', '1001', '1001', '1111'],
     v: ['10001', '10001', '10001', '01010', '00100'],
     w: ['10101', '10101', '10101', '10101', '01110'],
     x: ['10001', '01010', '00100', '01010', '10001'],
     y: ['10001', '01010', '00100', '00100', '00100'],
-    z: ['11111', '00010', '00100', '01000', '11111']
+    z: ['11111', '00010', '00100', '01000', '11111'],
+    '!': [],
+    question: [],
+    comma: [],
+    dot: [],
+    colon: [],
+    semicolon: [],
 
 };
 
 
-
-let str = "berk kalp fuat kalp batu";
+let str = document.getElementById("string").getAttribute("str");
+// let str = "test";
+str = str.toLowerCase()
 let words = str.split(" ");
 
 function setup() {
     noCanvas();
     header = createDiv();
-    header.addClass("header");
+    header.id("headertext");
+    header.parent("sketch-holder")
     from_text_to_letter();
+
+
 }
 // let boxes = document.getElementsByTagName("input")
 
@@ -78,11 +90,14 @@ function from_text_to_letter() {
                         var check = createCheckbox();
                         check.addClass("check");
                         check.parent(td);
-                    } else {
+                    } else if (cell == 0) {
                         var td = createElement('td');
                         td.addClass("cell");
                         td.parent(tr);
-
+                    } else if (cell == "-") {
+                        var td = createElement('td');
+                        td.addClass("gap");
+                        td.parent(tr);
                     }
                 }
 
@@ -92,5 +107,14 @@ function from_text_to_letter() {
     }
 }
 
+let kat = random(1.2, 7);
+console.log(kat)
 
-function draw() {}
+function draw() {
+    const ge = selectAll("td")
+
+    for (let i = 0; i < ge.length; i++) {
+        ge[i]._rotate(i * kat);
+    }
+    // frameRate(4);
+}
