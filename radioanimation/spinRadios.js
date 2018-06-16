7a61e364d2ae2b7d1edba6c0343a650def4c6ed0
vortextemporum <berk.ozd@gmail.com>
2018-06-16 21:55:17 +0300

    yes

--- radioanimation/spinRadios.js
+++ radioanimation/spinRadios.js
@@ -1,13 +1,16 @@
 new p5();
 
 var radioCircle = [];
-var totalRadius = 200;
 var div;
 var canvas;
-var numCircles = round(random(1, 20))
-var numRadios = round(random(8, 20));
 var allRadios;
-var speed;
+var totalRadius = 200;
+var numCircles = round(random(1, 30));
+var numRadios = round(random(6, 50));
+
+var spinSpeed = round(random(1, 10));
+var fps = round(random(1, 10));
+
 
 class Radio {
     constructor(x, y, value) {
@@ -25,7 +28,6 @@ class Radio {
     //     this.y = this.y + my;
     //     this.button.position(this.x, this.y);
     // }
-
 }
 
 function drawCircle(Inset, Radius, value) {
@@ -38,13 +40,26 @@ function drawCircle(Inset, Radius, value) {
     }
 }
 
+function randomCheckedPattern() {
+    let remainder = round(random(2, numRadios));
 
+    for (baba = 0; baba < allRadios.length; baba++) {
+        if (baba % remainder == 1) {
+            allRadios[baba].checked = true;
+        }
+        else {
+            allRadios[baba].checked = false;
+        }
+    }
+    
+}
 
 function setup() {
     noCanvas();
     div = createDiv().id("container");
-    div.size(totalRadius * 2 + 16, totalRadius * 2 + 15)
-    div.position(windowWidth / 2 - totalRadius, windowHeight / 2 - totalRadius)
+    div.size(totalRadius * 2 + 16, totalRadius * 2 + 14.5);
+    div.position(windowWidth / 2 - totalRadius, windowHeight / 2 - totalRadius);
+
 
     for (p = 0; p < numCircles; p++) {
         let Inset = p / numCircles * totalRadius;
@@ -54,16 +69,14 @@ function setup() {
     }
 
     allRadios = document.querySelectorAll("input");
-    let remainder = round(random(2, numRadios));
-    for (baba = 0; baba < allRadios.length; baba++) {
-        if (baba % remainder == 1) {
-            allRadios[baba].checked = true;
-        }
-    }
 
-    speed = round(random(1, 10))
-    div.style("animation", "spin infinite " + speed + "s linear")
-    frameRate(30);
+    // todo: changing the direction and spinning speed of each row
+    div.style("animation", "spin infinite " + spinSpeed + "s linear")
+
+    // framerate determines the speed of the randomCheckedPattern function
+    frameRate(fps);
 }
 
-function draw() {}
+function draw() {
+    randomCheckedPattern()
+}
